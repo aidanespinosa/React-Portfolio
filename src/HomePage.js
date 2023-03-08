ef8ee1c4e8af9dfa7b9a391fdd4117bcd3488d64
aidanespinosa <aidanespinosa3@gmail.com>
2023-03-08 15:16:23 -0800

    updated readme

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./HomePage.css";
 import Navigation from "./Navigation";
 
